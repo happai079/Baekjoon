029124b99338fb84ae3fae6d5904a58b661c9762
Hayoung Park <76716519+happai079@users.noreply.github.com>
2022-07-07 17:39:47 +0900

    [Bronze III] Title: 기찍 N, Time: 256 ms, Memory: 18448 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.js
@@ -0,0 +1,9 @@
+const input = Number(require('fs').readFileSync('/dev/stdin').toString());
+
+let result = '';
+
+for(let i = input; i >=1; i-- ) {
+    result += i + '\n';
+}
+
+console.log(result);
